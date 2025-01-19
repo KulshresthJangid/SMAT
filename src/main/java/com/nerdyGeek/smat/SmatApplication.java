738aa680a6135407c03b17afa330d405a24ed5de
Kulshresth Jangid <kulshresth.jangid@rampwin.com>
2025-01-19 14:29:35 +0530

    First commiut

---
+++ src/main/java/com/nerdyGeek/smat/SmatApplication.java
@@ -0,0 +1,13 @@
+package com.nerdyGeek.smat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerdyGeek/smat/SmatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdyGeek.smat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
