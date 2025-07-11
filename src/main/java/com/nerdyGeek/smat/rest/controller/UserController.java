b5e577ccef586fe2e5cb14bf8bbe70ff61d633a4
Kulshresth Jangid <kulshresthjangid@Kulshresths-MacBook-Pro.local>
2025-07-11 17:40:21 +0530

    added rtable configs

---
+++ src/main/java/com/nerdyGeek/smat/rest/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.nerdyGeek.smat.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/user")
+public class UserController {
+
+
+
+}
